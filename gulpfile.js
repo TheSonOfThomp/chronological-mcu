5817d88853b9643fa8a03013cb611f71b5d93546
Adam Thompson <athompson@newvisions.org>
2020-06-11 22:54:47 -0400

    Adds build process

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { src, dest, parallel, series } = require('gulp');
+const minify = require('gulp-minify');
+const zip = require('gulp-zip');
+
+const packjs = () => src(['src/*.js'])
+  .pipe(minify({ 
+    noSource: true, 
+    ext: {min: '.js'}
+  }))
+  .pipe(dest('build/'))
+
+const packhtml = () => src(['src/*.html']).pipe(dest('build/'));
+
+const packcss = () => src(['src/*.css']).pipe(dest('build/'));
+
+const copyManifest = () => src(['src/manifest.json']).pipe(dest('build/'))
+
+const copyImg = () => src(['src/images/**/*']).pipe(dest('build/images'))
+
+const zipBuild = () => src('build').pipe(zip('build.zip')).pipe(dest('./'))
+
+exports.default = series(
+  parallel(
+    packjs,
+    packhtml,
+    packcss,
+    copyManifest,
+    copyImg
+  ),
+  zipBuild
+)
